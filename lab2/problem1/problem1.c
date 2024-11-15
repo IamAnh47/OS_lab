290b2c7d724b7e67b21ba7096f29f66248af349f
Tuan Anh <anh.phanitskyye@hcmut.edu.vn>
2024-11-15 21:48:47 +0700

    Anh

--- lab2/problem1/problem1.c
+++ lab2/problem1/problem1.c
@@ -1,71 +1,90 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
-#include <unistd.h>
-#include <sys/wait.h>
 #include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
 
-#define SHM_KEY 12345
-#define MAX_MOVIES 1682
+#define SHARED_MEM_NAME "/movie_ratings"
+#define MAX_MOVIES 2000
+#define FILE1 "movie-100k_1.txt"
+#define FILE2 "movie-100k_2.txt"
 
 typedef struct {
-    int movie_id;
-    double total_rating;
-    int count;
-} MovieRating;
+    int movie_count[MAX_MOVIES];
+    double movie_sum[MAX_MOVIES];
+    int process_done[2];
+} SharedData;
 
-void calculate_average(const char *filename, MovieRating *shared_data) {
+void process_file(const char *filename, SharedData *shared_data, int process_id) {
     FILE *file = fopen(filename, "r");
     if (!file) {
         perror("Failed to open file");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 
-    int user_id, movie_id, rating, timestamp;
-    while (fscanf(file, "%d %d %d %d", &user_id, &movie_id, &rating, &timestamp) != EOF) {
-        shared_data[movie_id].movie_id = movie_id;
-        shared_data[movie_id].total_rating += rating;
-        shared_data[movie_id].count += 1;
-    }
-
-    fclose(file);
-}
-
-int main() {
-    int shm_id = shmget(SHM_KEY, sizeof(MovieRating) * MAX_MOVIES, IPC_CREAT | 0666);
-    if (shm_id < 0) {
-        perror("Failed to create shared memory");
-        exit(1);
-    }
-
-    MovieRating *shared_data = (MovieRating *) shmat(shm_id, NULL, 0);
-    memset(shared_data, 0, sizeof(MovieRating) * MAX_MOVIES);
-
-    if (fork() == 0) {
-        calculate_average("movie-100k_1.txt", shared_data);
-        exit(0);
-    }
-
-    if (fork() == 0) {
-        calculate_average("movie-100k_2.txt", shared_data);
-        exit(0);
-    }
-
-
-    wait(NULL);
-    wait(NULL);
-
-
-    for (int i = 1; i < MAX_MOVIES; i++) {
-        if (shared_data[i].count > 0) {
-            double avg_rating = shared_data[i].total_rating / shared_data[i].count;
-            printf("Movie ID %d: Average Rating = %.2f\n", shared_data[i].movie_id, avg_rating);
+    char line[256];
+    while (fgets(line, sizeof(line), file)) {
+        int userID, movieID, rating;
+        long timestamp;
+        if (sscanf(line, "%d\t%d\t%d\t%ld", &userID, &movieID, &rating, &timestamp) == 4) {
+            shared_data->movie_sum[movieID] += rating;
+            shared_data->movie_count[movieID]++;
         }
     }
 
-    shmdt(shared_data);
-    shmctl(shm_id, IPC_RMID, NULL);
+    fclose(file);
+    shared_data->process_done[process_id] = 1; // Mark this process as done
+}
+
+int main() {
+    int fd = shm_open(SHARED_MEM_NAME, O_CREAT | O_RDWR, 0666);
+    if (fd == -1) {
+        perror("shm_open failed");
+        exit(EXIT_FAILURE);
+    }
+
+    ftruncate(fd, sizeof(SharedData)); // Set the size of shared memory
+    SharedData *shared_data = mmap(NULL, sizeof(SharedData), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if (shared_data == MAP_FAILED) {
+        perror("mmap failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Initialize shared data
+    memset(shared_data, 0, sizeof(SharedData));
+
+    pid_t pid1 = fork();
+    if (pid1 == 0) {
+        // First child process
+        process_file(FILE1, shared_data, 0);
+        exit(EXIT_SUCCESS);
+    }
+
+    pid_t pid2 = fork();
+    if (pid2 == 0) {
+        // Second child process
+        process_file(FILE2, shared_data, 1);
+        exit(EXIT_SUCCESS);
+    }
+
+    // Parent process waits for children
+    wait(NULL);
+    wait(NULL);
+
+    // Calculate and print average ratings
+    printf("MovieID\tAverageRating\n");
+    for (int i = 0; i < MAX_MOVIES; i++) {
+        if (shared_data->movie_count[i] > 0) {
+            double average = shared_data->movie_sum[i] / shared_data->movie_count[i];
+            printf("%d\t%.2f\n", i, average);
+        }
+    }
+
+    // Clean up
+    munmap(shared_data, sizeof(SharedData));
+    shm_unlink(SHARED_MEM_NAME);
 
     return 0;
 }
