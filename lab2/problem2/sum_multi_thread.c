8d8e0663de8fd87e6a1bd0ef8b6ad9e5f16655fe
Tuan Anh <anh.phanitskyye@hcmut.edu.vn>
2024-11-15 22:26:57 +0700

    Anh

--- lab2/problem2/sum_multi_thread.c
+++ lab2/problem2/sum_multi_thread.c
@@ -3,49 +3,68 @@
 #include <pthread.h>
 
 typedef struct {
-    int start;
-    int end;
-    unsigned long long sum;
-} SumRange;
+    unsigned long long start;
+    unsigned long long end;
+    unsigned long long partial_sum;
+} ThreadData;
 
-void *calculate_sum(void *arg) {
-    SumRange *range = (SumRange *)arg;
-    range->sum = 0;
-    for (int i = range->start; i <= range->end; i++) {
-        range->sum += i;
+// Hàm do từng luồng thực hiện để tính tổng [start, end]
+void *calculate_partial_sum(void *arg) {
+    ThreadData *data = (ThreadData *)arg;
+    data->partial_sum = 0;
+    for (unsigned long long i = data->start; i <= data->end; i++) {
+        data->partial_sum += i;
     }
     return NULL;
 }
 
-unsigned long long sum_multi_thread(int n, int num_threads) {
-    pthread_t threads[num_threads];
-    SumRange ranges[num_threads];
-    int range_size = n / num_threads;
-    unsigned long long total_sum = 0;
-
-    for (int i = 0; i < num_threads; i++) {
-        ranges[i].start = i * range_size + 1;
-        ranges[i].end = (i == num_threads - 1) ? n : (i + 1) * range_size;
-        pthread_create(&threads[i], NULL, calculate_sum, &ranges[i]);
-    }
-
-    for (int i = 0; i < num_threads; i++) {
-        pthread_join(threads[i], NULL);
-        total_sum += ranges[i].sum;
-    }
-
-    return total_sum;
-}
-
 int main(int argc, char *argv[]) {
     if (argc != 3) {
-        fprintf(stderr, "Usage: %s <num_threads> <n>\n", argv[0]);
+        printf("Usage: %s <numThreads> <n>\n", argv[0]);
         return 1;
     }
 
-    int num_threads = atoi(argv[1]);
-    int n = atoi(argv[2]);
-    unsigned long long result = sum_multi_thread(n, num_threads);
-    printf("Sum is: %llu\n", result);
+    int numThreads = atoi(argv[1]);
+    unsigned long long n = atoll(argv[2]);
+
+    if (numThreads <= 0 || n <= 0) {
+        printf("Both numThreads and n must be positive.\n");
+        return 1;
+    }
+
+    pthread_t threads[numThreads];
+    ThreadData thread_data[numThreads];
+
+    unsigned long long range = n / numThreads;
+    unsigned long long remaining = n % numThreads;
+
+    // Chia đoạn và tạo các luồng
+    unsigned long long start = 1;
+    for (int i = 0; i < numThreads; i++) {
+        thread_data[i].start = start;
+        thread_data[i].end = start + range - 1;
+
+        // Chia phần dư cho các luồng đầu tiên
+        if (remaining > 0) {
+            thread_data[i].end += 1;
+            remaining--;
+        }
+
+        start = thread_data[i].end + 1;
+
+        if (pthread_create(&threads[i], NULL, calculate_partial_sum, &thread_data[i]) != 0) {
+            perror("Error creating thread");
+            return 1;
+        }
+    }
+
+    // Đợi các luồng hoàn thành
+    unsigned long long total_sum = 0;
+    for (int i = 0; i < numThreads; i++) {
+        pthread_join(threads[i], NULL);
+        total_sum += thread_data[i].partial_sum;
+    }
+
+    printf("Sum of numbers from 1 to %llu is: %llu\n", n, total_sum);
     return 0;
 }
--- lab2/problem2/sum_serial.c
+++ lab2/problem2/sum_serial.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-unsigned long long sum_serial(int n) {
-    unsigned long long sum = 0;
-    for (int i = 1; i <= n; i++) {
-        sum += i;
-    }
-    return sum;
+// Tinh tong tu 1 den n
+unsigned long long sum_serial(unsigned long long n) {
+    // unsigned long long sum = 0;
+    // for (unsigned long long i = 1; i <= n; i++) {
+    //     sum += i;
+    // }
+    // return sum;
+    return (n * (n + 1)) / 2; 
 }
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
-        fprintf(stderr, "Usage: %s <n>\n", argv[0]);
+        printf("Usage: %s <n>\n", argv[0]);
         return 1;
     }
 
-    int n = atoi(argv[1]);
+    unsigned long long n = atoll(argv[1]);
     unsigned long long result = sum_serial(n);
-    printf("Sum is: %llu\n", result);
+
+    printf("Sum of numbers from 1 to %llu is: %llu\n", n, result);
     return 0;
 }
