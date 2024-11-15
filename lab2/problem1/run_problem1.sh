efdf3a0a9f712821ed6b05e2da3a72ecb1000e36
Tuan Anh <anh.phanitskyye@hcmut.edu.vn>
2024-11-15 22:50:32 +0700

    Anh

--- lab2/problem1/run_problem1.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-gcc -o problem1 problem1.c -lrt
-
-./problem1
--- lab2/problem4/problem4.c
+++ lab2/problem4/problem4.c
@@ -2,86 +2,60 @@
 #include <stdlib.h>
 #include <string.h>
 #include <fcntl.h>
-#include <sys/mman.h>
 #include <unistd.h>
+#include <sys/mman.h>
 #include <sys/wait.h>
 
-#define FILE_NAME "shared_memory.dat"
-#define FILE_SIZE 1024 // 1KB shared memory
-
-void writer_process() {
-    // Mở hoặc tạo file
-    int fd = open(FILE_NAME, O_RDWR | O_CREAT, 0666);
-    if (fd == -1) {
-        perror("Failed to open file");
-        exit(EXIT_FAILURE);
-    }
-
-    // Mở rộng file tới kích thước yêu cầu
-    if (ftruncate(fd, FILE_SIZE) == -1) {
-        perror("Failed to set file size");
-        close(fd);
-        exit(EXIT_FAILURE);
-    }
-
-    // Map file vào vùng nhớ
-    char *mapped = mmap(NULL, FILE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-    if (mapped == MAP_FAILED) {
-        perror("Failed to mmap");
-        close(fd);
-        exit(EXIT_FAILURE);
-    }
-    close(fd); 
-
-    // Ghi dữ liệu vào vùng nhớ
-    const char *message = "Hello from writer process!";
-    snprintf(mapped, FILE_SIZE, "%s", message);
-    printf("Writer: Wrote message to shared memory: %s\n", message);
-
-    sleep(5);
-
-    munmap(mapped, FILE_SIZE);
-}
-
-void reader_process() {
-
-    int fd = open(FILE_NAME, O_RDONLY);
-    if (fd == -1) {
-        perror("Failed to open file");
-        exit(EXIT_FAILURE);
-    }
-
-    // Map file vào vùng nhớ
-    char *mapped = mmap(NULL, FILE_SIZE, PROT_READ, MAP_SHARED, fd, 0);
-    if (mapped == MAP_FAILED) {
-        perror("Failed to mmap");
-        close(fd);
-        exit(EXIT_FAILURE);
-    }
-    close(fd); 
-
-    printf("Reader: Read message from shared memory: %s\n", mapped);
-
-    // Unmap vùng nhớ
-    munmap(mapped, FILE_SIZE);
-}
+#define FILE_NAME "shared_file.txt"
+#define FILE_SIZE 1024 
 
 int main() {
+    int fd = open(FILE_NAME, O_RDWR | O_CREAT | O_TRUNC, 0666);
+    if (fd == -1) {
+        perror("Error opening file");
+        exit(EXIT_FAILURE);
+    }
+
+    if (ftruncate(fd, FILE_SIZE) == -1) {
+        perror("Error setting file size");
+        close(fd);
+        exit(EXIT_FAILURE);
+    }
+
+    char *mapped_data = mmap(NULL, FILE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if (mapped_data == MAP_FAILED) {
+        perror("Error mapping file");
+        close(fd);
+        exit(EXIT_FAILURE);
+    }
+    close(fd); 
+
     pid_t pid = fork();
-    if (pid == -1) {
-        perror("Failed to fork");
+    if (pid < 0) {
+        perror("Fork failed");
         exit(EXIT_FAILURE);
     }
 
     if (pid == 0) {
-        sleep(1); 
-        reader_process();
+        const char *message = "Hello from child process!\n";
+        snprintf(mapped_data, FILE_SIZE, "%s", message);
+        printf("Child: Wrote to shared memory: %s", message);
+        exit(EXIT_SUCCESS);
     } else {
-        writer_process();
-        wait(NULL); 
-    }
+        wait(NULL);
 
-    unlink(FILE_NAME);
+        printf("Parent: Read from shared memory: %s", mapped_data);
+
+        if (munmap(mapped_data, FILE_SIZE) == -1) {
+            perror("Error unmapping memory");
+            exit(EXIT_FAILURE);
+        }
+
+        if (remove(FILE_NAME) == -1) {
+            perror("Error removing file");
+            exit(EXIT_FAILURE);
+        }
+    }
 
     return 0;
 }
