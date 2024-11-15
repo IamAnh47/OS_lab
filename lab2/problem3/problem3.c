a425c4412000178d2308e83b34cd4d15ad7fb087
Tuan Anh <anh.phanitskyye@hcmut.edu.vn>
2024-11-15 22:34:58 +0700

    Anh

--- lab2/problem3/problem3.c
+++ lab2/problem3/problem3.c
@@ -1,68 +1,104 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/ipc.h>
-#include <sys/msg.h>
 #include <pthread.h>
+#include <mqueue.h>
 #include <unistd.h>
+#include <fcntl.h>
 
-#define MSG_KEY_1 12345  
-#define MSG_KEY_2 54321  
+#define QUEUE_A_TO_B "/queue_a_to_b"
+#define QUEUE_B_TO_A "/queue_b_to_a"
+#define MAX_MSG_SIZE 256
+#define MAX_MESSAGES 10
 
-#define MSG_SIZE 100
+// Hàm gửi tin nhắn từ Thread A đến Thread B
+void *thread_a_func(void *arg) {
+    mqd_t mq_send = mq_open(QUEUE_A_TO_B, O_WRONLY);
+    mqd_t mq_receive = mq_open(QUEUE_B_TO_A, O_RDONLY);
 
+    if (mq_send == -1 || mq_receive == -1) {
+        perror("Thread A: mq_open failed");
+        pthread_exit(NULL);
+    }
 
-struct message {
-    long msg_type;
-    char msg_text[MSG_SIZE];
-};
+    char send_buffer[MAX_MSG_SIZE];
+    char recv_buffer[MAX_MSG_SIZE];
 
+    for (int i = 0; i < 5; i++) {
+        snprintf(send_buffer, MAX_MSG_SIZE, "Message %d from A", i + 1);
+        mq_send(mq_send, send_buffer, strlen(send_buffer) + 1, 0);
 
-void* thread1_func(void* arg) {
-    int msgid_send = msgget(MSG_KEY_1, 0666 | IPC_CREAT);
-    int msgid_recv = msgget(MSG_KEY_2, 0666 | IPC_CREAT);
-    
-    struct message msg;
-    msg.msg_type = 1;
-    strcpy(msg.msg_text, "Hello from Thread 1");
+        printf("Thread A sent: %s\n", send_buffer);
 
-    printf("Thread 1: Sending message to Thread 2\n");
-    msgsnd(msgid_send, &msg, sizeof(msg), 0);
+        mq_receive(mq_receive, recv_buffer, MAX_MSG_SIZE, NULL);
+        printf("Thread A received: %s\n", recv_buffer);
+    }
 
-    msgrcv(msgid_recv, &msg, sizeof(msg), 1, 0);
-    printf("Thread 1: Received message from Thread 2: %s\n", msg.msg_text);
-
-    msgctl(msgid_send, IPC_RMID, NULL);
-    msgctl(msgid_recv, IPC_RMID, NULL);
-
-    return NULL;
+    mq_close(mq_send);
+    mq_close(mq_receive);
+    pthread_exit(NULL);
 }
 
-void* thread2_func(void* arg) {
-    int msgid_recv = msgget(MSG_KEY_1, 0666 | IPC_CREAT);
-    int msgid_send = msgget(MSG_KEY_2, 0666 | IPC_CREAT);
-    
-    struct message msg;
+// Hàm gửi tin nhắn từ Thread B đến Thread A
+void *thread_b_func(void *arg) {
+    mqd_t mq_receive = mq_open(QUEUE_A_TO_B, O_RDONLY);
+    mqd_t mq_send = mq_open(QUEUE_B_TO_A, O_WRONLY);
 
-    msgrcv(msgid_recv, &msg, sizeof(msg), 1, 0);
-    printf("Thread 2: Received message from Thread 1: %s\n", msg.msg_text);
+    if (mq_send == -1 || mq_receive == -1) {
+        perror("Thread B: mq_open failed");
+        pthread_exit(NULL);
+    }
 
-    msg.msg_type = 1;
-    strcpy(msg.msg_text, "Hello from Thread 2");
-    printf("Thread 2: Sending reply to Thread 1\n");
-    msgsnd(msgid_send, &msg, sizeof(msg), 0);
+    char send_buffer[MAX_MSG_SIZE];
+    char recv_buffer[MAX_MSG_SIZE];
 
-    return NULL;
+    for (int i = 0; i < 5; i++) {
+        mq_receive(mq_receive, recv_buffer, MAX_MSG_SIZE, NULL);
+        printf("Thread B received: %s\n", recv_buffer);
+
+        snprintf(send_buffer, MAX_MSG_SIZE, "Reply %d from B", i + 1);
+        mq_send(mq_send, send_buffer, strlen(send_buffer) + 1, 0);
+
+        printf("Thread B sent: %s\n", send_buffer);
+    }
+
+    mq_close(mq_receive);
+    mq_close(mq_send);
+    pthread_exit(NULL);
 }
 
 int main() {
-    pthread_t thread1, thread2;
+    // Thiết lập thuộc tính hàng đợi
+    struct mq_attr attr = {
+        .mq_flags = 0,
+        .mq_maxmsg = MAX_MESSAGES,
+        .mq_msgsize = MAX_MSG_SIZE,
+        .mq_curmsgs = 0,
+    };
 
-    pthread_create(&thread1, NULL, thread1_func, NULL);
-    pthread_create(&thread2, NULL, thread2_func, NULL);
+    // Tạo hàng đợi
+    mqd_t mq1 = mq_open(QUEUE_A_TO_B, O_CREAT | O_RDWR, 0666, &attr);
+    mqd_t mq2 = mq_open(QUEUE_B_TO_A, O_CREAT | O_RDWR, 0666, &attr);
 
-    pthread_join(thread1, NULL);
-    pthread_join(thread2, NULL);
+    if (mq1 == -1 || mq2 == -1) {
+        perror("Main: mq_open failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Tạo hai luồng
+    pthread_t thread_a, thread_b;
+    pthread_create(&thread_a, NULL, thread_a_func, NULL);
+    pthread_create(&thread_b, NULL, thread_b_func, NULL);
+
+    // Chờ các luồng kết thúc
+    pthread_join(thread_a, NULL);
+    pthread_join(thread_b, NULL);
+
+    // Xóa hàng đợi
+    mq_close(mq1);
+    mq_close(mq2);
+    mq_unlink(QUEUE_A_TO_B);
+    mq_unlink(QUEUE_B_TO_A);
 
     return 0;
 }
